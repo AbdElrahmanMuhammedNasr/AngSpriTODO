2520fabb15773d3809b13a23a52f0d81e653a916
AbdElrahmanMuhammedNasr <abdo300nasr@gmail.com>
2019-10-13 08:08:16 +0100

    inti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { TodoOPComponent } from './todo-op/todo-op.component';
+import { routing } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HttpInterService } from './service/InterAuth/http-inter.service';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoOPComponent,
+    HomeComponent,
+    LoginFormComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    // {provide: HTTP_INTERCEPTORS, useClass: HttpInterService, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
